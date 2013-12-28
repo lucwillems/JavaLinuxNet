c6c3444ee77757e4e6fb53d26201ff19d5219d38
luc <luc.willems@it4y.eu>
2013-12-28 10:03:50 +0100

    basic test structure

---
+++ src/java/test/org/it4y/net/protocols/testICMPPacket.java
@@ -0,0 +1,7 @@
+package org.it4y.net.protocols;
+
+/**
+ * Created by luc on 12/28/13.
+ */
+public class testICMPPacket {
+}
---
+++ src/java/test/org/it4y/net/testRawPacket.java
@@ -0,0 +1,7 @@
+package org.it4y.net;
+
+/**
+ * Created by luc on 12/28/13.
+ */
+public class testRawPacket {
+}
