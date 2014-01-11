6d9bfcd1e5e89320cc3b1bed7e6bb27e7d779cec
luc <luc.willems@it4y.eu>
2014-01-11 10:29:44 +0100

    - remove unneeded files

--- src/native/build-headers.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-#
-CLASSPATH=../../target/classes/
-javah -classpath $CLASSPATH org.it4y.jni.tuntap
-javah -classpath $CLASSPATH org.it4y.jni.tproxy
-javah -classpath $CLASSPATH org.it4y.jni.linuxutils
-javah -classpath $CLASSPATH org.it4y.jni.libnetlink3
