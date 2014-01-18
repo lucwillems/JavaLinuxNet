6db94131769e74730b5f72adee0df346a71b2679
luc <luc.willems@it4y.eu>
2014-01-18 12:53:50 +0100

    - clean up system.out

--- src/main/java/org/it4y/demo/DemoTestApp.java
+++ src/main/java/org/it4y/demo/DemoTestApp.java
@@ -3,6 +3,8 @@ package org.it4y.demo;
 import org.it4y.net.link.LinkManager;
 import org.it4y.net.link.LinkNotification;
 import org.it4y.net.link.NetworkInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by luc on 12/25/13.
@@ -14,37 +16,32 @@ import org.it4y.net.link.NetworkInterface;
  * and now ping to 8.8.4.4
  */
 public class DemoTestApp {
+    private static Logger log= LoggerFactory.getLogger("Demo");
 
     public static void main(String[] args) throws Exception {
-        //System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
-
-        TNetlinkRoutingListener router = new TNetlinkRoutingListener();
-        router.start();
-
-        System.out.println("netlink listener running");
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
         TProxyListener tproxy = new TProxyListener();
         tproxy.start();
-        System.out.println("tproxy server running");
+        log.info("tproxy server running");
         Thread.sleep(100);
         TunTapInterfaceListener tundev = new TunTapInterfaceListener("test", 1500);
         tundev.start(); //this will bring interface luc UP
-        System.out.println("tun interface listener running");
+        log.info("tun interface listener running");
         LinkManager lnkMng=new LinkManager();
         lnkMng.registerListener(LinkNotification.EventAction.None,LinkNotification.EventType.All, new LinkNotification() {
             @Override
             public void onEvent(EventAction action, EventType type, NetworkInterface network) {
-                System.out.println("onEvent : "+action+" "+type+" "+network);
+                log.info("onEvent : "+action+" "+type+" "+network);
             }
 
             @Override
             public void onStateChanged(NetworkInterface network) {
-                System.out.println("onStateChanged : "+network+" active:"+network.isActive());
+                log.info("onStateChanged : "+network+" active:"+network.isActive());
             }
         });
         lnkMng.start();
-        //multi threaded access
-        //TestThread tt=new TestThread(lnkMng);
-        //tt.start();
+        Thread.sleep(500);
+        log.info("ready to rock and roll");
         while (true) {
             Thread.sleep(1000);
             tundev.dumpSpeed();
