2c1944b9418902406aa73f38d8c0170b63bd8e78
luc <luc.willems@it4y.eu>
2014-01-18 11:30:50 +0100

    - basic junit test for tundevice, should be able to run on any linux

---
+++ src/test/java/org/it4y/jni/tunDeviceTest.java
@@ -0,0 +1,129 @@
+package org.it4y.jni;
+
+import junit.framework.Assert;
+import org.it4y.net.tuntap.TunDevice;
+import org.junit.Test;
+
+/**
+ * Created by luc on 1/18/14.
+ * This test can not do mutch when no admin rights have been given.
+ * It willhow ever :
+ * - load / call jni stuff and make sure this is ok
+ * - check security in case it is run with security not active
+ * - incase setcap is used, we can test jni calls
+ *
+ * the integration test is more pratical, but needs additional setup .
+ */
+
+public class tunDeviceTest {
+
+    /* create and open a tunnel device */
+    private TunDevice openTun(String device) throws libc.ErrnoException {
+        TunDevice tun=null;
+        if (device != null) {
+            tun=new TunDevice(device);
+        } else {
+            tun=new TunDevice();
+        }
+        Assert.assertNotNull(tun);
+        return tun;
+    }
+
+    @Test
+    public void testOpenTunDevice() throws Exception {
+        TunDevice tun=null;
+        try {
+            tun=openTun("test");
+            try {
+                tun.open();
+            } catch (libc.ErrnoException errno) {
+                //must be "Operation not permitted
+                Assert.assertEquals(1,errno.getErrno());
+            }
+        } finally {
+            if (tun != null) {
+                tun.close();
+                Assert.assertEquals(0,tun.getFd());
+            }
+        }
+    }
+
+    @Test
+    public void testOpenTun() throws Exception {
+        TunDevice tun=null;
+        try {
+            tun=openTun(null);
+            try {
+                tun.open();
+            } catch (libc.ErrnoException errno) {
+                //must be "Operation not permitted
+                Assert.assertEquals(1,errno.getErrno());
+            }
+        } finally {
+            if (tun != null) {
+                tun.close();
+                Assert.assertEquals(0,tun.getFd());
+            }
+        }
+    }
+
+
+    @Test
+    public void testSetNonBlocking() throws Exception {
+        TunDevice tun=null;
+        try {
+            tun=openTun(null);
+            try {
+                tun.open();
+                tun.setNonBlocking(true);
+             } catch (libc.ErrnoException errno) {
+                //must be "Operation not permitted
+                Assert.assertEquals(1,errno.getErrno());
+            }
+        } finally {
+            if (tun != null) {
+                tun.close();
+                Assert.assertEquals(0,tun.getFd());
+            }
+        }
+
+    }
+
+    @Test
+    public void testIsDataReady() throws Exception {
+            TunDevice tun=null;
+            try {
+                try {
+                    tun=openTun(null);
+                    tun.isDataReady(100);
+                } catch (libc.ErrnoException errno) {
+                    //must be "Operation not permitted
+                    Assert.assertEquals(1,errno.getErrno());
+                }
+            } finally {
+                if (tun != null) {
+                    tun.close();
+                    Assert.assertEquals(0,tun.getFd());
+                }
+            }
+    }
+
+    @Test
+    public void testEnableQueue() throws Exception {
+        TunDevice tun=null;
+        try {
+            try {
+                tun=openTun(null);
+                tun.enableQueue(false);
+            } catch (libc.ErrnoException errno) {
+                //must be "Operation not permitted
+                Assert.assertEquals(1,errno.getErrno());
+            }
+        } finally {
+            if (tun != null) {
+                tun.close();
+                Assert.assertEquals(0,tun.getFd());
+            }
+        }
+    }
+}
