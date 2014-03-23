57cd32728e1a0ad83416b0a28f3e239e2a677c55
luc <luc.willems@it4y.eu>
2014-03-23 08:07:25 +0100

    - extra netlink tests

--- src/test/java/org/it4y/net/netlink/NlMessageTest.java
+++ src/test/java/org/it4y/net/netlink/NlMessageTest.java
@@ -25,6 +25,7 @@ public class NlMessageTest {
         Assert.assertEquals(0,msg.getNlMsgSequence());
         Assert.assertEquals(0,msg.getNlmsg_flags());
         Assert.assertEquals(-1,msg.getRTAIndex(""));
+        Assert.assertNull(msg.createRTAMessage(0,rawData));
         Assert.assertNotNull(msg.toString());
         logger.info("NlErrorMessage : {}",msg.toString());
     }
@@ -40,6 +41,7 @@ public class NlMessageTest {
         Assert.assertEquals(0,msg.getNlMsgSequence());
         Assert.assertEquals(0,msg.getNlmsg_flags());
         Assert.assertEquals(-1,msg.getRTAIndex(""));
+        Assert.assertNull(msg.createRTAMessage(0,rawData));
         Assert.assertNotNull(msg.toString());
         logger.info("NlErrorMessage : {}",msg.toString());
     }
