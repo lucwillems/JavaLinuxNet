0f482e13d7c6a2292bb9f75e8ca869274f8b3f8d
luc <luc.willems@it4y.eu>
2014-03-30 11:35:58 +0200

    - update license to Apache v2

--- src/main/java/org/it4y/jni/linuxutils.java
+++ src/main/java/org/it4y/jni/linuxutils.java
@@ -1,8 +1,17 @@
 /*
- * This file is derived from Linux kernel headers
- * All copyrights preserved to original kernel author.
+ * Copyright 20124 Luc Willems (T.M.M.)
  *
- * more info see https://www.kernel.org/pub/linux/kernel/COPYING
+ * We licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.it4y.jni;
--- src/main/java/org/it4y/net/link/LinkNotification.java
+++ src/main/java/org/it4y/net/link/LinkNotification.java
@@ -1,10 +1,17 @@
 /*
+ * Copyright 20124 Luc Willems (T.M.M.)
  *
- *  * Copyright (C) 2014  Luc.Willems @ T.M.M.
- *  *
- *  * This file is released under the LGPL.
- *  * see license.txt for terms and conditions
+ * We licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
  *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.it4y.net.link;
